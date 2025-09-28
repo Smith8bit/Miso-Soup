0938a96f317b8206fa636a91d3ed91043810f706
Smith8bit <thesmithdot@gmail.com>
2025-09-28 21:36:32 +0700

    test split and merge

--- soup.py
+++ soup.py
@@ -3,7 +3,6 @@ from selenium_stealth import stealth
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from itertools import zip_longest
 import time
 
 options = webdriver.ChromeOptions()
@@ -31,35 +30,19 @@ hotel_list = {}
 time.sleep(20)
 wait = WebDriverWait(driver, 10)
 
-#for i in range(0, 10):
-#    names = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'c9Hnq-hotel-name')))
-#    ratings = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//div[contains(@class, 'Dp6Q')]")))
-#    classes = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//span[contains(@class, 'hEI8')]")))
-#    prices = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//div[contains(@class, 'c1XBO')]")))
-#    locations = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//div[contains(@class, 'upS4')]")))
-#    for j in range(len(names)):
-#        hotel_list[names[j].text] = [ratings[j].text, classes[j].text, prices[j].text, locations[j].text]
-
-for i in range(0, 10):
-    names = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'c9Hnq-hotel-name')))
-    ratings = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//div[contains(@class, 'Dp6Q')]")))
-    classes = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//span[contains(@class, 'hEI8')]")))
-    prices = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//div[contains(@class, 'c1XBO')]")))
-    locations = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//div[contains(@class, 'upS4')]")))
-    print(f"page {i+1} have {len(names)} {len(ratings)} {len(classes)} {len(prices)} {len(locations)}")
+# for i in range(0, 10):
+#     names = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'c9Hnq-hotel-name')))
+#     ratings = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//div[contains(@class, 'Dp6Q')]")))
+#     classes = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//span[contains(@class, 'hEI8')]")))
+#     prices = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//div[contains(@class, 'c1XBO')]")))
+#     locations = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//div[contains(@class, 'upS4')]")))
+#     print(f"page {i+1} have {len(names)} {len(ratings)} {len(classes)} {len(prices)} {len(locations)}")
     
-    for name, rating, cls, price, location in zip_longest(names, ratings, classes, prices, locations, fillvalue=None):
-        hotel_list[name.text if name else "Unknown Hotel"] = [
-            rating.text if rating else "N/A",
-            cls.text if cls else "N/A",
-            price.text if price else "N/A",
-            location.text if location else "N/A"
-        ]
+#     driver.find_element(By.XPATH, "//button[@aria-label='Next page']").click()
 
-
-    driver.find_element(By.XPATH, "//button[@aria-label='Next page']").click()
-
-# for name, detail in hotel_list.items():
-#     print(name, detail)
+test_classes = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "DOkx")))
+for item in test_classes:
+    a,b,c = item.text.split('\n')
+    print(f"Review rating: {a} Review score: {b} Class: {c}")
 
 driver.quit()
