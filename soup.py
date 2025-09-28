aef77efcfcb73e78b2cd2ac82bc571df99bea15b
Smith8bit <thesmithdot@gmail.com>
2025-09-28 22:26:57 +0700

    make soup become big fucking function

--- soup.py
+++ soup.py
@@ -4,12 +4,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import sqlite3
 
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
 
-# options.add_argument("--headless")
+options.add_argument("--headless")
 
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
@@ -24,36 +23,36 @@ stealth(driver,
         fix_hairline=True,
         )
 
+def get_a_thing_done(URL):
+    url = str(URL)
+    # url = "https://www.kayak.com/hotels/Tokyo,Tokyo-Prefecture,Japan-c21033/2026-01-01/2026-01-02/1adults"
+    driver.get(url)
 
+    hotel_list = {}
+    time.sleep(20)
+    wait = WebDriverWait(driver)
 
-url = "https://www.kayak.com/hotels/Tokyo,Tokyo-Prefecture,Japan-c21033/2026-01-01/2026-01-02/1adults"
-driver.get(url)
+    for i in range(10):
+        # ratings = []
+        # scores = []
+        # stars =[]
+        names = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'c9Hnq-hotel-name')))
+        # reviews = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "DOkx")))
+        # for review in reviews:
+        #     rinfo = review.text.split('\n')
+        #     if len(rinfo) == 2:
+        #         rr, rs = rinfo
+        #         star = "0 stars"
+        #     if len(rinfo) == 3:
+        #         rr, rs, star = rinfo
+        #     ratings.append(rr)
+        #     scores.append(rs)
+        #     stars.append(star)
+        prices = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//div[contains(@class, 'c1XBO')]")))
+        # locations = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//div[contains(@class, 'upS4')]")))
+        driver.find_element(By.XPATH, "//button[@aria-label='Next page']").click()
+        for j in len(names):
+            hotel_list[names[j].text] = prices[j].text 
+    driver.quit()
 
-hotel_list = {}
-time.sleep(20)
-wait = WebDriverWait(driver)
-
-for i in range(100):
-    ratings = []
-    scores = []
-    stars =[]
-    names = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'c9Hnq-hotel-name')))
-    reviews = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "DOkx")))
-    for review in reviews:
-        rinfo = review.text.split('\n')
-        if len(rinfo) == 2:
-            rr, rs = rinfo
-            star = "0 stars"
-        if len(rinfo) == 3:
-            rr, rs, star = rinfo
-        ratings.append(rr)
-        scores.append(rs)
-        stars.append(star)
-    prices = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//div[contains(@class, 'c1XBO')]")))
-    locations = wait.until(EC.presence_of_all_elements_located((By.XPATH, ".//div[contains(@class, 'upS4')]")))
-    print(f"page {i+1} have {len(names)} {len(ratings)} {len(scores)} {len(stars)} {len(prices)} {len(locations)}")
-    
-    driver.find_element(By.XPATH, "//button[@aria-label='Next page']").click()
-
-    
-driver.quit()
+    return hotel_list
